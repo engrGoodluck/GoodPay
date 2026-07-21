101810ce884a8ab18deb85d413f9f7650fdbcbbe
Goodluck <goodluckemeka65@gmail.com>
2026-07-21 19:12:44 +0100

    Completed Dasboard

--- screens/dashboard/script.js
+++ screens/dashboard/script.js
@@ -3,15 +3,15 @@ const eye = document.getElementById("toggleBalance");
 
 let visible = true;
 
-eye.addEventListener("click", function(){
+eye.addEventListener("click", function () {
 
-    if(visible){
+    if (visible) {
 
         balance.textContent = "••••••••••";
         eye.src = "../../assets/icons/eye-off.svg";
         visible = false;
 
-    }else{
+    } else {
 
         balance.textContent = "₦378,765,030.00";
         eye.src = "../../assets/icons/eye.svg";
@@ -20,3 +20,21 @@ eye.addEventListener("click", function(){
     }
 
 });
+
+const greeting = document.getElementById("greeting");
+
+const hour = new Date().getHours();
+
+if (hour < 12) {
+
+    greeting.textContent = "Good Morning 👋";
+
+} else if (hour < 18) {
+
+    greeting.textContent = "Good Afternoon ☀️";
+
+} else {
+
+    greeting.textContent = "Good Evening 🌙";
+
+}
