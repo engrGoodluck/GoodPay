6dcede9be02790e6ae7cc8f33f4f43450713881d
engrGoodluck <goodluckemeka65@gmail.com>
2026-07-30 11:43:06 -0700

    Added user registration and login logic

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const users = [];
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -44,11 +46,58 @@ app.post("/register", (req, res) => {
         });
     }
 
+    users.push(user);
+
     res.json({
         success: true,
         message: "User registered successfully!",
         data: user
     });
+});
+
+app.post("/login", (req, res) => {
+
+    const { email, password } = req.body;
+
+    const user = users.find((user) => user.email === email);
+
+    if (!user) {
+    return res.json({
+        success: false,
+        message: "User not found."
+    });
+}
+
+if (user.password !== password) {
+    return res.json({
+        success: false,
+        message: "Incorrect password."
+    });
+}
+
+    if (!email) {
+        return res.json({
+            success: false,
+            message: "Email is required."
+        });
+    }
+
+    if (!password) {
+        return res.json({
+            success: false,
+            message: "Password is required."
+        });
+    }
+
+    res.json({
+        success: true,
+        message: "Login successful!"
+    });
+
+});
+
+app.get("/users", (req, res) => {
+    res.json(users);
 });
 
 app.listen(3000, () => {
